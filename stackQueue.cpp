3b16a5340d672370b26ade50c41390c90bee726c
mmscibor <mmscibor@gmail.com>
2014-03-13 15:14:04 -0400

    First commit

---
+++ stackQueue.cpp
@@ -0,0 +1,15 @@
+/* Michael Scibor
+ * This project creates Stack and Queue objects which can be used
+ * to push() and pop() objects. Both of these classes were created
+ * from scratch without utilizing C++ built in Collections. */
+
+#include<iostream>
+
+
+
+int main() {
+    std::string directory;
+
+    std::cout << "Specify the .txt file directory: ";
+    std::cin >> directory;
+}
